5793faf332269db9e91ff8dd0452192a18187968
bikramdhami0100 <bikramdhami334@gmail.com>
2025-04-16 13:48:35 +0545

    push

--- app/api/route.js
+++
@@ -1,5 +0,0 @@
-import { NextResponse } from 'next/server';
-export default function GET() {
-    return NextResponse.json({ message: 'WebSocket server is running' });
-  // Next.js requires a default export
-}
